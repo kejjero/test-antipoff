08a428201ea0523f0582a5629e291406d2fdbd9a
Peters Maxim <74023694+kejjero@users.noreply.github.com>
2022-11-10 21:26:54 +0800

    feat!: added view width for window

--- src/redux/base/baseSlice.ts
+++ src/redux/base/baseSlice.ts
@@ -4,6 +4,7 @@ import {initialStateBase} from "./types"
 
 const initialState: initialStateBase = {
     activeBase: true,
+    width: 1280,
 }
 
 const baseSlice = createSlice({
@@ -13,9 +14,13 @@ const baseSlice = createSlice({
         setChangeBase(state, action) {
             state.activeBase = action.payload
         },
+        getCurrentWidth(state, action) {
+            state.width = action.payload
+        }
     },
 });
 
-export const {setChangeBase} = baseSlice.actions;
+export const {setChangeBase, getCurrentWidth} = baseSlice.actions;
 export default baseSlice.reducer;
 export const selectBase = (state: RootState) => state.baseReducer
+export const selectWidth = (state: RootState) => state.baseReducer.width
